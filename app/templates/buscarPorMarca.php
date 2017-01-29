feced15a39da430f61cdc466e58d37e73bb27c2f
Sebas Sebas <seplarui@gmail.com>
2017-01-29 20:57:22 +0100

    buscarPorMarca.php

---
+++ app/templates/buscarPorMarca.php
@@ -0,0 +1,29 @@
+<?php ob_start()?>
+
+<form name="formBusqueda" action="index.php?ctl=" buscar" method="post">
+
+	<table>
+	
+	<tr>
+	<td>
+	<input type="text" name="marca" value="<?php echo $parametro['marca'];?>">Puedes utilizar comodín "%".
+	</td>
+	<td>
+	<input type="submit" value="Buscar"></td>
+	</tr>
+	</table>
+</form>
+
+<?php foreach ($parametros['resultado'] as $marca):?>
+<tr>
+
+<td><a href="index.php?ctl=ver&id=<?php echo $alimento['id']?>">
+<?php $marca['marca']?></a></td>
+
+</tr>
+<?php endforeach;?>
+</table>
+
+<?php $contenido=ob_get_clean();?>
+<?php include 'layout.php'?>
+
