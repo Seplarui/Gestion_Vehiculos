558ed9508f8a56d6b23419a2ce30132d0bff7a38
Sebas Sebas <seplarui@gmail.com>
2017-01-29 11:12:22 +0100

    verMarca.php

--- app/Model.php
+++ app/Model.php
@@ -1,1 +1,31 @@
 <?php
+
+
+//app/Model.php
+
+include_once("Marca.php");
+
+class Model {
+	
+	protected $conexion;
+	
+	public function __construct($dbname, $dbuser,$dbpas,$dbhost) {
+		
+		
+		$this->conexion=NULL;
+		
+		try {
+			
+			$bdconexion=new PDO('mysql:host'.$dbhost.';dbname='.$dbname.';charset=utf8',$dbuser,$dbpass);
+			
+			$this->conexion=$bdconexion;
+			
+		} catch (PDOException $e) {
+			echo 'Error: '.$e->getMessage();
+		}
+		
+		
+	}
+	
+	
+}
