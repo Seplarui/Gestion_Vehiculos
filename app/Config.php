bf49767a4bba4d62a0077a918e3164b67043f8d5
Sebas Sebas <seplarui@gmail.com>
2017-01-29 11:34:19 +0100

    Ficheros

--- app/Config.php
+++ app/Config.php
@@ -1,1 +1,8 @@
 <?php
+class Config {
+	public static $bd_hostname = "localhost";
+	public static $bd_nombre = "marcas";
+	public static $bd_usuario = "alumno";
+	public static $bd_clave = "alumno";
+	public static $mvc_vis_csS = "estilo.css";
+}
--- app/Model.php
+++ app/Model.php
@@ -1,31 +1,104 @@
 <?php
 
-
-//app/Model.php
-
-include_once("Marca.php");
-
+// app/Model.php
+include_once ("Marca.php");
 class Model {
-	
 	protected $conexion;
-	
-	public function __construct($dbname, $dbuser,$dbpas,$dbhost) {
-		
-		
-		$this->conexion=NULL;
+	public function __construct($dbname, $dbuser, $dbpas, $dbhost) {
+		$this->conexion = NULL;
 		
 		try {
 			
-			$bdconexion=new PDO('mysql:host'.$dbhost.';dbname='.$dbname.';charset=utf8',$dbuser,$dbpass);
+			$bdconexion = new PDO ( 'mysql:host' . $dbhost . ';dbname=' . $dbname . ';charset=utf8', $dbuser, $dbpass );
 			
-			$this->conexion=$bdconexion;
+			$this->conexion = $bdconexion;
+		} catch ( PDOException $e ) {
+			echo 'Error: ' . $e->getMessage ();
+		}
+	}
+	public function dameMarca() {
+		$consulta = "select * from marcas";
+		
+		$resultado = $this->conexion->query ( $consulta );
+		
+		$marcas = array ();
+		$cont = 0;
+		
+		$filas = $resultado->fetchAll ( PDO::FETCH_OBJ );
+		
+		foreach ( $filas as $fila ) {
 			
-		} catch (PDOException $e) {
-			echo 'Error: '.$e->getMessage();
+			$marca = new Marca ( $fila->id, $fila->marca );
+			$marcas [$cont] = $marca;
+			$cont ++;
 		}
 		
+		$conexion = false;
+		return $marcas;
+	}
+	public function buscarMarcaPorMarca($marca) {
+		$marca = htmlspecialchars ( $marca );
 		
+		$sql = "select * from marcas where marca like'" . $marca;
+		$resultado = $this->conexion->query ( $sql );
+		$marcas = array ();
+		$cont = 0;
+		
+		$filas = $resultado->fetchAll ( PDO::FETCH_OBJ );
+		foreach ( $filas as $fila ) {
+			$marca = new Marca ( $fila->id, $fila->marca );
+			$marcas [$cont] = $marca;
+			$cont ++;
+		}
+		
+		$conexion = false;
+		return $marcas;
+	}
+	public function dameMarca($id) {
+		$consulta = "select * from marcas where id=:id;";
+		$resultado = $this->conexion->prepare ( $consulta );
+		$resultado = execute ( array (
+				":id" => $id 
+		) );
+		$marcas = array ();
+		$cont = 0;
+		
+		$fila = $resultado->fetch ( PDO::FETCH_OBJ );
+		
+		$marca = new Marca ( $fila->id, $fila->marca );
+		$conexion = false;
+		
+		return $marca;
+	}
+	public function insertarMarca($marca) {
+		try {
+			$sql = "insert into marcas(marca) values('.$marca')";
+			
+			$resultado = $this->conexion->prepare ( $sql );
+			
+			if (! $resultado) {
+				
+				print_r ( $this->conexion->errorInfo () );
+			}
+			
+			$count = $result->execute ( array (
+					":marca" => $marca 
+			) );
+		} catch ( PDOException $e ) {
+			echo 'Error: ' . $e->getMessage ();
+			return false;
+		}
+		
+		$conexion = false;
+		if ($count == 1) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
-	
+	public function validarDatos($marca) {
+		$valido=is_string($marca);
+		return ($valido);
+	}
 }
